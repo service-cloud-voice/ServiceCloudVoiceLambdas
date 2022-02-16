cbe01c9fadf34653eb4e0ad59dc4facc72063291
Jinal Kathiara <jkathiara@salesforce.com>
2022-02-16 14:16:09 -0800

    remove contact lens consumer lambda

--- contactLensConsumer/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  audience: "https://scrt.salesforce.com", // The audience specified in the claim of the generated JWT token.
-  orgId: process.env.SALESFORCE_ORG_ID,
-  scrtEndpointBase: process.env.SCRT_ENDPOINT_BASE,
-  privateKeyParamName: process.env.PRIVATE_KEY_PARAM_NAME,
-  callCenterApiName: process.env.CALL_CENTER_API_NAME,
-  tokenValidFor: "5m" // JWT token valid duration.
-};
