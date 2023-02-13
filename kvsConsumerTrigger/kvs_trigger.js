dd57eb90d7d8fec204d88ba5b39b3756e3ef1a0c
Peter Mescalchin <peter@magnetikonline.com>
2023-02-14 10:43:38 +1100

    Fix environment variable formatting for CTR data sync function (#29)

--- kvsConsumerTrigger/kvs_trigger.js
+++ kvsConsumerTrigger/kvs_trigger.js
@@ -75,7 +75,7 @@ exports.handler = (event, context, callback) => {
 
   const params = {
     // not passing in a ClientContext
-    FunctionName: process.env.transcriptionFunction,
+    FunctionName: process.env.INVOKE_KVS_TRANSCRIBER_ARN,
     // InvocationType is RequestResponse by default
     // LogType is not set so we won't get the last 4K of logs from the invoked function
     // Qualifier is not set so we use $LATEST
